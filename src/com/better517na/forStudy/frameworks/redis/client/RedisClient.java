58d00fa2b973e289e0fdf765484237440b4231f5
JateLiao <374682617@qq.com>
2017-03-16 19:23:26 +0800

    redis

---
+++ src/com/better517na/forStudy/frameworks/redis/client/RedisClient.java
@@ -0,0 +1,33 @@
+/*
+ * 文件名：RedisClient.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： RedisClient.java
+ * 修改人：tianzhong
+ * 修改时间：2017年3月16日
+ * 修改内容：新增
+ */
+package com.better517na.forStudy.frameworks.redis.client;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     tianzhong
+ */
+public class RedisClient {
+
+    public static void main(String[] args) {
+
+        JedisPool jedisPool = null;
+        Jedis jedis = jedisPool.getResource();
+        // jedis.
+    }
+}
