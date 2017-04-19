c2d9e7df912f68dc1b9bf63434d49fdd82b66c1b
JateLiao <374682617@qq.com>
2017-04-19 18:07:30 +0800

    validate

---
+++ src/util/ValidateUtil.java
@@ -0,0 +1,24 @@
+/*
+ * 文件名：ValidateUtil.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： ValidateUtil.java
+ * 修改人：tianzhong
+ * 修改时间：2017年4月19日
+ * 修改内容：新增
+ */
+package util;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     tianzhong
+ */
+public class ValidateUtil {
+
+}
