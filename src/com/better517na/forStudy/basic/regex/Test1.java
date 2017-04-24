77f28f17e864f589e30d8e7cbe19034326745c23
JateLiao <374682617@qq.com>
2017-04-24 10:14:15 +0800

    regex

---
+++ src/com/better517na/forStudy/basic/regex/Test1.java
@@ -0,0 +1,30 @@
+/*
+ * 文件名：Test1.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： Test1.java
+ * 修改人：tianzhong
+ * 修改时间：2017年4月24日
+ * 修改内容：新增
+ */
+package com.better517na.forStudy.basic.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * @author     tianzhong
+ */
+public class Test1 {
+
+    @Test
+    public void test1(){
+        String str = "    @hah";
+        Pattern p = Pattern.compile("\\s+\\@");
+        Matcher m = p.matcher(str);
+        if (m.find()) {
+            System.out.println(str);
+        }
+    }
+}
