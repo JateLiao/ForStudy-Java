5dade97ba761c83666bafe048a4ea743f404b7fc
JateLiao <374682617@qq.com>
2017-06-09 15:58:16 +0800

    final

---
+++ src/com/better517na/forStudy/quicktest/test/on2017_06_09_final/FinalTest1.java
@@ -0,0 +1,24 @@
+/*
+ * 文件名：FinalTest1.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： FinalTest1.java
+ * 修改人：tianzhong
+ * 修改时间：2017年6月9日
+ * 修改内容：新增
+ */
+package com.better517na.forStudy.quicktest.test.on2017_06_09_final;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     tianzhong
+ */
+public class FinalTest1 {
+
+}
