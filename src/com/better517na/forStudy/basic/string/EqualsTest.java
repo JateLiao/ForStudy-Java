871ebf6019a1bfce6344afeb56dbcc0db6c77987
JateLiao <374682617@qq.com>
2017-09-05 18:36:33 +0800

    equals Test

---
+++ src/com/better517na/forStudy/basic/string/EqualsTest.java
@@ -0,0 +1,30 @@
+/*
+ * 文件名：EqualsTest.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： EqualsTest.java
+ * 修改人：tianzhong
+ * 修改时间：2017年9月5日
+ * 修改内容：新增
+ */
+package com.better517na.forStudy.basic.string;
+
+import java.util.Objects;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * 
+ * <pre>
+ * </pre>
+ * 
+ * @author tianzhong
+ */
+public class EqualsTest {
+    public static void main(String[] args) {
+        double deductFeesBefore = 0.0;
+        System.out.println(Objects.equals(0.0, deductFeesBefore));
+    }
+}
