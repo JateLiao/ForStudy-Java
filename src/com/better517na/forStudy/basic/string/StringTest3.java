a1660eb34a3fab9941eaf7b1316c358f54d9a9ca
JateLiao <374682617@qq.com>
2017-09-07 08:58:37 +0800

    regex

--- src/com/better517na/forStudy/basic/string/StringTest3.java
+++ src/com/better517na/forStudy/basic/string/StringTest3.java
@@ -9,6 +9,8 @@
 package com.better517na.forStudy.basic.string;
 
 import java.text.MessageFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
@@ -51,6 +53,30 @@ public class StringTest3 {
         // System.out.println(s.split("或"));
         // System.out.println(s.split("或|或者"));
         // System.out.println(s.split("或者|或"));
+    }
+    
+    @Test
+    public void test4(){
+        String[] arr = {"1.8*2或2张(1.5*2)",
+                "1张1.2*2或1张2.2*2",
+                "-2*2+2张1.2*2.0",
+                "2.6米圆床",
+                "1.2*2*2+1.8*2",    
+                "1.2*2或1.8*2+1.2*2",   
+                "1.5*2+2张1.2*2",   
+                "1.8*2+1.3*2+1.3*2",
+                "3张1.2*2或1.8*2+1.2*2",    
+                "3张1.2*2或1.8*2+1张1.2*2",  
+                "1.35*2.0两张",  
+                "1张1.8*2或者2张1.2*2",
+                "1张1.5*2+2张1.2*2"};
+        Pattern p = Pattern.compile("[0-9](\\.[0-9]{1,2})?(\\.[0-9]{1,2})?(\\*|x|X)[0-9](\\.[0-9]{1,2})?(\\.[0-9]{1,2})?((\\*|x|X)[0-9](\\.[0-9]{1,2})?(\\.[0-9]{1,2})?)?");
+        for (String bedSize : arr) {
+            Matcher m = p.matcher(bedSize);
+            while (m.find()) {
+                System.out.println(m.group(0)); 
+            }
+        }
     }
     
 }
